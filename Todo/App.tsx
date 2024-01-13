1dedcd888a96f39a25af5a4a78f2784767ff26ce
djliu9049890 <55365026+djliu9049890@users.noreply.github.com>
2024-01-13 13:40:48 -0500

    initial app w/ not working firestore

--- Todo/App.tsx
+++ Todo/App.tsx
@@ -1,11 +1,12 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
+ * double tap r to reload changes
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import firestore from '@react-native-firebase/firestore';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -13,106 +14,62 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const [text, setText] = useState('')
+  const onSubmitPress = () => {
+    console.log(text, "sfdksmd")
+  }
+  const ref = firestore().collection('todos');
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style = {style.mainContainer}>
+      <View style = {{flexDirection: 'row'}}> 
+        <TextInput
+        value = {text}
+        onChangeText = {setText}
+        style = {style.textInput}
+        />
+        
+        <TouchableOpacity 
+        onPress = {onSubmitPress}
+        style = {style.button}>
+            <Text style = {{color: 'red'}}>
+              Submit
+            </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const style=StyleSheet.create({
+  textInput: {
+    backgroundColor: 'green',
+    width: '70%',
+    height: 50,
+    color: 'white',
+    borderRadius: 5
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  mainContainer: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 20
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  button: {
+    width: 90,
+    height: 50, 
+    backgroundColor: 'yellow',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10
+  }
+})
 
 export default App;
