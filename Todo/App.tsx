0eacb37d852524d65d0822b1f1e67e797fe80d8e
djliu9049890 <55365026+djliu9049890@users.noreply.github.com>
2024-01-23 13:40:55 -0500

    working app

--- Todo/App.tsx
+++ Todo/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import firestore from '@react-native-firebase/firestore';
 import type {PropsWithChildren} from 'react';
 import {
@@ -18,14 +18,50 @@ import {
   TouchableOpacity,
   useColorScheme,
   View,
+  Alert,
+  FlatList
 } from 'react-native';
 
+interface ListItem {
+  //id: string;
+  title: string;
+  complete: boolean;
+  // Add other properties if necessary
+}
+
 function App(): React.JSX.Element {
-  const [text, setText] = useState('')
-  const onSubmitPress = () => {
-    console.log(text, "sfdksmd")
-  }
+  const [text, setText] = useState('');
   const ref = firestore().collection('todos');
+  const [list, setList] = useState<ListItem[]>([]);
+
+  useEffect(()=>{
+    return ref.onSnapshot(querySnapshot => {
+      const list: ListItem[] = [];
+      querySnapshot.forEach(doc => {
+        list.push({
+          //id: doc.data().id,
+          title: doc.data().title,
+          complete: doc.data().complete
+        })
+      })
+      setList(list)
+    })
+  },[])
+
+  const onSubmitPress = async() => {
+    console.log(text, "sfdksmd")
+    if (text.length == 0) {
+      Alert.alert("please enter todo");
+      return
+    }
+    await ref.add({
+      title: text,
+      complete: false
+    })
+    console.log(text)
+    setText('')
+  }
+  console.log(list)
 
   return (
     <View style = {style.mainContainer}>
@@ -44,11 +80,30 @@ function App(): React.JSX.Element {
             </Text>
         </TouchableOpacity>
       </View>
+      <FlatList
+        data = {list}
+        renderItem = {({item}) => (
+          <View style = {style.card}>
+            <Text style = {{color: 'white'}}>{item.title}</Text>
+            <Text style = {{color: 'white'}}>{item.complete? 'complete': 'not complete'}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const style=StyleSheet.create({
+  card: {
+    width: '100%',
+    height: 40,
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+    paddingHorizontal: 10
+  },
   textInput: {
     backgroundColor: 'green',
     width: '70%',
